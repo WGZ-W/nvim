ec8273888461aa6d006f5a7bf1be74f0a20a2a4a
wgz-mac <371960029@qq.com>
2024-10-09 16:20:00 +0800

    first commit

---
+++ lua/options.lua
@@ -0,0 +1,20 @@
+
+
+
+vim.o.relativenumber = true
+vim.o.number = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.wrap = true
+vim.o.cursorline = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.splitright = true
+vim.o.splitbelow = true
+-- vim.o.mouse:append("a")
+-- vim.o.clipboard:append("unnamedplus")
+
+
+
