118ea7e5b3ae2de8b954c190a70fbaa5315dad5c
wgz-mac <371960029@qq.com>
2024-10-10 14:22:37 +0800

    Add nvim-cmp and everforest plugin

--- lua/options.lua
+++ lua/options.lua
@@ -13,8 +13,8 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.splitright = true
 vim.o.splitbelow = true
+-- vim.o
 -- vim.o.mouse:append("a")
 -- vim.o.clipboard:append("unnamedplus")
-
 
 
